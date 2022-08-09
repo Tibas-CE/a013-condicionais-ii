1c243a0a269b02e95845829877f6f46ba53b9d02
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-08-09 20:40:13 -0300

    Adição de conteúdo

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -8,22 +8,31 @@ if(num % 2 == 0){
     } else {
         console.log("Este número e divisível apenas por 2");
     }
+} else if(num % 3 == 0){
+    console.log("Este número e divisível apenas por 3");
 } else{
-    console.log("Não foi dessa vez.");
+    console.log("Não foi dessa vez!");
 };
 
 //Utilizando um operador lógico para unir duas operações relacionai
 
 //operador &&
+
 if(num % 2 == 0 && num % 3 == 0){
     console.log("Este número e divisível por 2 e 3");
+} else if(num % 2 == 0){
+    console.log("Este número é divisível por 2");
+} else if(num % 3 == 0){
+    console.log("Este número e divisível apenas por 3");
 } else{
-    console.log("Este número não é divisível por 2 e 3");
+    console.log("Não foi dessa vez!");
 };
 
 //operador ||
+
 if(num % 2 == 0 || num % 3 == 0){
-    console.log("Este número e divisível por 2 e 3");
-} else{
-    console.log("Este número não é divisível por 2 e 3");
+    console.log("Este número e divisível por 2 ou 3");
+}   else {
+    console.log("Não foi dessa vez!");
 };
+
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,4 +1,4 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+//let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
 /*if(nacionalidade === "brasileira"){
     console.log("a pessoa é do Brasil!");
@@ -16,22 +16,51 @@ let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
 //utilizando o switch-case
 
+let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+
 switch (nacionalidade) {
-    case "brasileira":
+    case "brasileira", "brasileiro":
         console.log("a pessoa é do Brasil!");
         break;
-    case "argentina":
+    case "argentina", "argentino":
         console.log("a pessoa é da Argentina!");
         break;
-    case "uruguaia":
+    case "uruguaia", "uruguaio":
         console.log("a pessoa é do Uruguai!");
         break;
-    case "chilena":
+    case "chilena", "chileno":
         console.log("a pessoa é do Chile!");
         break;
-    case "colombiana":
+    case "colombiana", "colombiano":
         console.log("a pessoa é da Colômbia!");
         break;
     default:
         console.log("Nacionalidade não encontrada");    
-}
+};
+
+//utilizando switch-case com array
+/*let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+
+let nacao = [
+    "brasileira", "brasileiro", "argentina", "argentino", "chilena", "chileno", "colombiana", "colombiano" 
+];
+
+switch (nacionalidade) {
+    case nacao [0 || 1]:
+        console.log("a pessoa é do Brasil!");
+        break;
+    case nacionalidade [2 || 3]:
+        console.log("a pessoa é da Argentina!");
+        break;
+    case nacionalidade [4 || 5]:
+        console.log("a pessoa é do Uruguai!");
+        break;
+    case nacionalidade [6 || 7]:
+        console.log("a pessoa é do Chile!");
+        break;
+    case nacionalidade [8 || 9]:
+        console.log("a pessoa é da Colômbia!");
+        break;
+    default:
+        console.log("Nacionalidade não encontrada");    
+};*/
